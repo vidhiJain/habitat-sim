3c52f7100de78519c5c85ae57b48f70e64fb1e50
Eric Undersander <eundersander@gmail.com>
2021-06-18 15:23:11 -0700

    add Arrange Recorder readme and arrange_recorder.sh

---
+++ examples/cpp_apps/arrange_recorder/arrange_recorder.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+../../../build_debug/utils/viewer/viewer "data/lighthouse_kitchen_dataset/scenes/scene0.scene_instance.json" --dataset "data/lighthouse_kitchen_dataset/lighthouse_kitchen.scene_dataset_config.json" --enable-physics --disable-navmesh --stage-requires-lighting
