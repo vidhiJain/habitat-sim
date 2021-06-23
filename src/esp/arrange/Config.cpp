c461884d16133bfde4fa8903563e11a4bc3668ad
Eric Undersander <eundersander@gmail.com>
2021-06-22 18:29:51 -0700

    updates to arrange Config

--- src/esp/arrange/Config.cpp
+++ src/esp/arrange/Config.cpp
@@ -8,6 +8,69 @@ namespace esp {
 namespace arrange {
 
 /*
+struct ConfigArticulatedLink {
+  float divisionEps = 1e-2f;
+  float distEps = 0.02f;
+  float maxVelMag = 20.f;
+  float baseVelMag0 = 0.2f;
+  float baseVelMag1 = 0.05f;
+  float rampUpTime = 2.0f;
+};
+*/
+
+esp::io::JsonGenericValue toJsonValue(const ConfigArticulatedLink& x,
+                                      esp::io::JsonAllocator& allocator) {
+  esp::io::JsonGenericValue obj(rapidjson::kObjectType);
+  esp::io::addMember(obj, "divisionEps", x.divisionEps, allocator);
+  esp::io::addMember(obj, "distEps", x.distEps, allocator);
+  esp::io::addMember(obj, "maxVelMag", x.maxVelMag, allocator);
+  esp::io::addMember(obj, "baseVelMag0", x.baseVelMag0, allocator);
+  esp::io::addMember(obj, "baseVelMag1", x.baseVelMag1, allocator);
+  esp::io::addMember(obj, "rampUpTime", x.rampUpTime, allocator);
+  return obj;
+}
+
+bool fromJsonValue(const esp::io::JsonGenericValue& obj,
+                   ConfigArticulatedLink& x) {
+  esp::io::readMember(obj, "divisionEps", x.divisionEps);
+  esp::io::readMember(obj, "distEps", x.distEps);
+  esp::io::readMember(obj, "maxVelMag", x.maxVelMag);
+  esp::io::readMember(obj, "baseVelMag0", x.baseVelMag0);
+  esp::io::readMember(obj, "baseVelMag1", x.baseVelMag1);
+  esp::io::readMember(obj, "rampUpTime", x.rampUpTime);
+  return true;
+}
+
+/*
+struct ConfigColors {
+  Magnum::Color4 goodPlacement = Mn::Color4(0.f, 1.0, 0.f, 1.f);
+  Magnum::Color4 badPlacement = Mn::Color4(1.f, 0.0, 0.f, 1.f);
+  Magnum::Color4 artObj = Mn::Color4(1.f, 0.5, 0.f, 1.f);
+  Magnum::Color4 rigidObj = Mn::Color4(1.f, 0.0, 0.f, 1.f);
+};
+*/
+
+esp::io::JsonGenericValue toJsonValue(const ConfigColorSet& x,
+                                      esp::io::JsonAllocator& allocator) {
+  esp::io::JsonGenericValue obj(rapidjson::kObjectType);
+  esp::io::addMember(obj, "goodPlacement", x.goodPlacement, allocator);
+  esp::io::addMember(obj, "badPlacement", x.badPlacement, allocator);
+  esp::io::addMember(obj, "artObj", x.artObj, allocator);
+  esp::io::addMember(obj, "rigidObj", x.rigidObj, allocator);
+  esp::io::addMember(obj, "placementGuide", x.placementGuide, allocator);
+  return obj;
+}
+
+bool fromJsonValue(const esp::io::JsonGenericValue& obj, ConfigColorSet& x) {
+  esp::io::readMember(obj, "goodPlacement", x.goodPlacement);
+  esp::io::readMember(obj, "badPlacement", x.badPlacement);
+  esp::io::readMember(obj, "artObj", x.artObj);
+  esp::io::readMember(obj, "rigidObj", x.rigidObj);
+  esp::io::readMember(obj, "placementGuide", x.placementGuide);
+  return true;
+}
+
+/*
 struct ConfigCamera {
   Magnum::Vector3 eye;
   Magnum::Vector3 lookat;
@@ -29,17 +92,47 @@ bool fromJsonValue(const esp::io::JsonGenericValue& obj, ConfigCamera& x) {
 
 /*
 struct Config {
+  // save a keyframe every n physics steps (see also Arranger::physicsTimestep_)
+  int keyframeSavePeriod = 4;
+  // see Bullet gDeactivationTime
+  float physicsDeactivationTime = 0.75f;
+  // During cursor-based placement, we search up for a collision-free placement
+  float placementSearchHeight = 0.5f;
+  // The mouse picker casts a sphere into the scene to start placement. A
+smaller
+  // radius gives you more control but makes it harder to place on thin objects
+  // like a dishwasher rack.
+  float pickerSphereRadius = 0.015f;
+  ConfigArticulatedLink link;
+  ConfigColorSet colors;
   std::vector<ConfigCamera> cameras;
-};*/
+};
+*/
 
 esp::io::JsonGenericValue toJsonValue(const Config& x,
                                       esp::io::JsonAllocator& allocator) {
   esp::io::JsonGenericValue obj(rapidjson::kObjectType);
+  esp::io::addMember(obj, "keyframeSavePeriod", x.keyframeSavePeriod,
+                     allocator);
+  esp::io::addMember(obj, "physicsDeactivationTime", x.physicsDeactivationTime,
+                     allocator);
+  esp::io::addMember(obj, "placementSearchHeight", x.placementSearchHeight,
+                     allocator);
+  esp::io::addMember(obj, "pickerSphereRadius", x.pickerSphereRadius,
+                     allocator);
+  esp::io::addMember(obj, "link", x.link, allocator);
+  esp::io::addMember(obj, "colors", x.colors, allocator);
   esp::io::addMember(obj, "cameras", x.cameras, allocator);
   return obj;
 }
 
 bool fromJsonValue(const esp::io::JsonGenericValue& obj, Config& x) {
+  esp::io::readMember(obj, "keyframeSavePeriod", x.keyframeSavePeriod);
+  esp::io::readMember(obj, "physicsDeactivationTime",
+                      x.physicsDeactivationTime);
+  esp::io::readMember(obj, "placementSearchHeight", x.placementSearchHeight);
+  esp::io::readMember(obj, "link", x.link);
+  esp::io::readMember(obj, "colors", x.colors);
   esp::io::readMember(obj, "cameras", x.cameras);
   return true;
 }
--- src/esp/arrange/Config.h
+++ src/esp/arrange/Config.h
@@ -14,23 +14,61 @@
 namespace esp {
 namespace arrange {
 
+// see Arranger::updateForLinkAnimation
+struct ConfigArticulatedLink {
+  float divisionEps = 1e-2f;
+  float distEps = 0.02f;
+  float maxVelMag = 20.f;
+  float baseVelMag0 = 0.2f;
+  float baseVelMag1 = 0.05f;
+  float rampUpTime = 2.0f;
+};
+
+struct ConfigColorSet {
+  Magnum::Color4 goodPlacement = Mn::Color4(0.f, 1.0, 0.f, 1.f);
+  Magnum::Color4 badPlacement = Mn::Color4(1.f, 0.0, 0.f, 1.f);
+  Magnum::Color4 artObj = Mn::Color4(1.f, 0.5, 0.f, 1.f);
+  Magnum::Color4 rigidObj = Mn::Color4(1.f, 0.0, 0.f, 1.f);
+  // set alpha to 0 to disable placementGuide
+  Magnum::Color4 placementGuide = Mn::Color4(0.f, 1.0, 0.f, 0.3f);
+};
+
 struct ConfigCamera {
   Magnum::Vector3 eye{Magnum::Math::ZeroInit};
   Magnum::Vector3 lookat{Magnum::Math::ZeroInit};
 };
 
 struct Config {
+  // save a keyframe every n physics steps (see also Arranger::physicsTimestep_)
+  int keyframeSavePeriod = 4;
+  // see Bullet gDeactivationTime
+  float physicsDeactivationTime = 0.75f;
+  // During cursor-based placement, we search up for a collision-free placement
+  float placementSearchHeight = 0.5f;
+  // The mouse picker casts a sphere into the scene to start placement. A
+  // smaller radius gives you more control but makes it harder to place on thin
+  // objects like a dishwasher rack.
+  float pickerSphereRadius = 0.015f;
+  ConfigArticulatedLink link;
+  ConfigColorSet colors;
   std::vector<ConfigCamera> cameras;
 };
 
+esp::io::JsonGenericValue toJsonValue(const ConfigArticulatedLink& x,
+                                      esp::io::JsonAllocator& allocator);
+bool fromJsonValue(const esp::io::JsonGenericValue& obj,
+                   ConfigArticulatedLink& x);
+
+esp::io::JsonGenericValue toJsonValue(const ConfigColorSet& x,
+                                      esp::io::JsonAllocator& allocator);
+bool fromJsonValue(const esp::io::JsonGenericValue& obj, ConfigColorSet& x);
+
 esp::io::JsonGenericValue toJsonValue(const ConfigCamera& x,
                                       esp::io::JsonAllocator& allocator);
-
 bool fromJsonValue(const esp::io::JsonGenericValue& obj, ConfigCamera& x);
 
 esp::io::JsonGenericValue toJsonValue(const Config& x,
                                       esp::io::JsonAllocator& allocator);
-
 bool fromJsonValue(const esp::io::JsonGenericValue& obj, Config& x);
 
 }  // namespace arrange
